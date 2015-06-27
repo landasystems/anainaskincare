acad085c53455652666b5f8a9abb1fd368144b67
x1n73ct <x1n73ct@x1n73ct>
2015-06-27 12:29:55 +0700

    Merge origin/master
    Conflicts:
    	js/config.router.js

--- js/controllers/cabang.js
+++ js/controllers/cabang.js
@@ -63,6 +63,9 @@ app.controller('cabangCtrl', function ($scope, Data, toaster) {
         });
     };
     $scope.cancel = function () {
+        if (!$scope.is_view){ //hanya waktu edit cancel, di load table lagi
+            $scope.callServer(tableStateRef);
+        }
         $scope.is_edit = false;
         $scope.is_view = false;
     };
