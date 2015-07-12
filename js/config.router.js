d159edc28653b29f30a5baf472020ffee5b8e5e1
asrulsibaoel <asrulsibaoel@Baoelz>
2015-07-12 09:41:46 +0700

    Merge origin/master
    Conflicts:
    	js/config.router.js

--- js/config.router.js
+++ js/config.router.js
@@ -264,7 +264,7 @@ angular.module('app')
                                     resolve: {
                                         deps: ['$ocLazyLoad',
                                             function ($ocLazyLoad) {
-                                                return $ocLazyLoad.load(['ui.select2', 'ng-bootstrap-datepicker']).then(
+                                                return $ocLazyLoad.load(['ui.select']).then(
                                                         function () {
                                                             return $ocLazyLoad.load('js/controllers/t_pembelian.js');
                                                         }
